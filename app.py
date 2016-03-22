48c0c9b3b724c2e8536819bb27258d13f11caf89
JunHyung Kim <kimjh@humaxdigital.com>
2016-03-22 11:37:58 +0900

    additional

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = flask.Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return flask.render_template('index.html')
+    return flask.render_template('base.html')
     
 if __name__ == "__main__":
     app.run(debug = True, host = "0.0.0.0", port = 5020)
